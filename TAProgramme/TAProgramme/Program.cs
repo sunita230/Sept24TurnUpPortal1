edb8c5cf4494932ceca9f67316a5783faabf58ce
SunitaTharani <sunitat@missionreadyhq.com>
2024-10-12 14:14:04 +1300

    Login and Build Create Time Record automated tests built

--- TAProgramme/TAProgramme/Program.cs
+++ TAProgramme/TAProgramme/Program.cs
@@ -21,7 +21,7 @@ public class Program
         IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
         //enter a value in the username Textbox
         usernameTextbox.SendKeys("hari");
-
+     
         //4. Identify password textbox and enter valid password
 
         //find the ID of the element from the website
@@ -49,5 +49,68 @@ public class Program
         {
             Console.WriteLine("User has not logged in successfully, Test failed!");
         };
+
+        //CREATE A "TIME" RECORD
+
+        //Navigate to Adminstration tab 
+        IWebElement adminstrationTab = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a/span"));
+        //Click on the Adminstration tab
+        adminstrationTab.Click();
+
+        //Navigate to Time and Material tab
+        IWebElement timeAndMaterialOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+        //Click on the Time and Material tab
+        timeAndMaterialOption.Click();
+
+        //Navigate to the "Create New" button
+        IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+        //Click on "Create New" button
+        createNewButton.Click();
+
+        //Find the TypeCode dropdown 
+        IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+        typeCodeDropdown.Click();
+        //Select "Time" from the dropdown TypeCode
+        IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+        timeOption.Click();
+
+        //Type the code into the Code textbox
+        IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+        codeTextbox.SendKeys("Test Code");
+
+        //Type description into the Description textbox
+        IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+        descriptionTextbox.SendKeys("Test Description");
+
+        //Type price into the Price textbox
+        IWebElement priceTagOverLap = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+        priceTagOverLap.Click();
+        
+        IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+        priceTextbox.SendKeys("200");
+
+        //Click on "Save" button
+        IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+        saveButton.Click();
+        Thread.Sleep(3000);
+
+        //CHECK IF THE TIME RECORD HAS BEEN CREATED SUCCESSFULLY
+
+        //Navigate to the last page of Time and Material
+        IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+        goToLastPageButton.Click();
+
+        IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+        if (newCode.Text == "Test Code")
+        {
+            Console.WriteLine("Time record created successfully");
+        }
+        else
+        {
+            Console.WriteLine("Time record have not been created");
+        };
+
+
     }
 }
